29e95da2231f3d118b3994ecd9072a329ced6974
abdoulayeniang2010 <abdoulayeniang2010@gmail.com>
2020-02-28 13:30:32 +0100

    integration de dialogue rudimentaire a tour de role entre le serveur et le client. on (client/serveur) envoie un message et attend la reponse. le message exit ferme la connexion

--- client.py
+++ client.py
@@ -1,16 +1,31 @@
 import socket, sys
 
 host = "localhost"
-port = 2233
-
+port = 4441
+#creation du socket
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
-    #on demande une connexion sur l'adresse et le port renseignés
+    #demande de connexion
     clientSocket.connect((host,port))
 except socket.error:
     print ("connexion echoué")
     sys.exit()
-# le client recoit et affiche le message du serveur avec la methode recv
+#recevoir le message serveur
 messageServeur = clientSocket.recv(2048).decode("Utf8")
-print ("serveur >> ",messageServeur)
+#dialogue a tour de role entre le serveur et le client
+while True:
+    #si le serveur envoie exit
+    if messageServeur.upper() == "EXIT":
+        clientSocket.send("exit".encode("Utf8"))
+        break
+    # on affiche le message serveur
+    print ("Serveur ## ",messageServeur)
+    #saisir et envoyer son message
+    messageClient = input("Client >> ")
+    clientSocket.send(messageClient.encode("Utf8"))
+    #recevoir le message
+    messageServeur = clientSocket.recv(2048).decode("Utf8")
+#fermeture de la connexion
+print("connexion interrompue")
+clientSocket.close()
--- serveur.py
+++ serveur.py
@@ -1,7 +1,7 @@
 import socket , sys
 
 host = "localhost"
-port = 2233
+port = 4441
 
 # creation du socket
 serveurSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -9,17 +9,35 @@ serveurSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
     #on associe le socket avec son adresse et un port d'ecoute
     serveurSocket.bind((host,port))
-
 except socket.error:
     #au cas l'adresse est incorrect ou le port est deja utilisé
     print("# WARNING: %s:%s a echoué"%(host,port))
     sys.exit () #on quitte le programme
 
 print ("serveur pret en attente de connexion")
-serveurSocket.listen(0)
-
+serveurSocket.listen(3)
 connexion, adresse = serveurSocket.accept()
 print ("client IP : %s port %s"%(adresse[0], adresse[1]))
 
+#envoie de mesage vers le client avec la methode send
 messageServeur = "connecté sur le serveur de Wakanda"
 connexion.send(messageServeur.encode("Utf8"))
+
+#dialogue a tour de role entre le client et le serveur
+messageClient = connexion.recv(2048).decode("Utf8")
+
+while True:
+    #affichage message du client
+    print("Client >> ",messageClient)
+
+    if messageClient.upper() == "EXIT":
+        break
+    #saisir et envoyer le message au climat
+    messageServeur = input("Serveur # ")
+    connexion.send(messageServeur.encode("Utf8"))
+    #le serveur recoit le message du client
+    messageClient = connexion.recv(2048).decode("Utf8")
+#envoie du message exit suivie du fermeture de la connexion
+connexion.send("exit".encode("Utf8"))
+print("connexion interrompue")
+connexion.close()
